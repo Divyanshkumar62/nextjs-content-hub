b9fc41457c0648f39ab4baf934f480d3f219a085
Divyansh Kumar <kumardivyansh62@gmail.com>
2025-06-16 23:40:08 +0530

    Refined the auth structure

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,5 +2,23 @@ import {betterAuth} from 'better-auth'
 
 export const auth = betterAuth({
     appName: 'Next.js 15 blog',
-    secret: process.env.BETTER_ATH_SECRET || 'BETTER_AUTH_SECRET'
+    secret: process.env.BETTER_ATH_SECRET || 'BETTER_AUTH_SECRET',
+    baseURL: process.env.BASE_URL,
+    emailAndPassword: {
+        enabled: true,
+        requireEmailVerification: false,
+        minPasswordLength: 6,
+        maxPasswordLength: 128,
+        autoSignIn: false
+    },
+    session: {
+        expiresIn: 60 * 60 * 24 * 7,
+        updateAge: 60 * 60 * 24,
+        cookieCache: {
+            enabled: true,
+            maxAge: 60 * 5
+        },
+        disableSessionRefresh: true,
+        
+    }
 })
